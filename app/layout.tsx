cec92165391aff5504d5c1e4a81fe99332428ce0
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-06-16 00:32:43 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,10 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav>This is Navigation</nav>
+        {children}
+      </body>
     </html>
   )
 }
